f696872f7cc89d5abbe3be9dfe119c45b91f4b08
Balaji J <balajijanarthanan1997@gmail.com>
2021-07-02 10:53:31 +0530

    Added User, UI Components and Styling

---
+++ src/components/Users/AddUser.jsx
@@ -0,0 +1,40 @@
+import React, { useState } from "react";
+import Card from "../UI/Card";
+import Button from "../UI/Button";
+import classes from "./AddUser.module.css";
+
+const AddUser = (props) => {
+  const [userName, setUserName] = useState("");
+  const [age, setAge] = useState("");
+
+  const addUserHandler = (event) => {
+    event.preventDefault();
+    console.log(userName, age);
+  };
+
+  const usernameChangeHandler = (event) => {
+    setUserName(event.target.value);
+  };
+
+  const ageChangeHandler = (event) => {
+    setAge(event.target.value);
+  };
+  return (
+    <Card className={classes.input}>
+      <form onSubmit={addUserHandler}>
+        <label htmlFor='username'>Username</label>
+        <input
+          id='username'
+          type='text'
+          value={userName}
+          onChange={usernameChangeHandler}
+        />
+        <label htmlFor='age'>Age (Years)</label>
+        <input id='age' type='number' value={age} onChange={ageChangeHandler} />
+        <Button type='submit'>Add User</Button>
+      </form>
+    </Card>
+  );
+};
+
+export default AddUser;
